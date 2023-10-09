f27780b6574b81b999c75d99d98cacc311594bd9
Brandon Essler <Essler@users.noreply.github.com>
2023-10-08 23:32:41 -0400

    Remove unused boilerplate methods

--- Balloon Popper/BalloonManager.gd
+++ Balloon Popper/BalloonManager.gd
@@ -5,16 +5,6 @@ var score : int = 0
 @export var score_text : Label
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func increase_score (amount):
 	score += amount
 	score_text.text = str("Score: ", score)
